437b310995f3add8699d1821ed8e68cf69361c00
ahmetanil1 <amtyrgnc@gmail.com>
2024-12-18 22:14:42 +0300

    Finished main.cpp

--- main.cpp
+++ main.cpp
@@ -1,60 +1,79 @@
 #include "function.h"
+#include <cstdio>
+#include <windows.h>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+void writeToFileAndPrint(StudentType students[], int studentCount, int choice, const string &fileName)
+{
+    // Dosyayı temizle
+    ofstream clearFile(fileName, ios::trunc);
+    clearFile.close();
+
+    // Öğrencileri terminale yazdır
+    for (int i = 0; i < studentCount; ++i)
+    {
+        students[i].print(choice);
+    }
+
+    // Öğrencileri dosyaya yazdır
+    for (int i = 0; i < studentCount; ++i)
+    {
+        students[i].print(choice, fileName);
+    }
+}
 
 int main()
 {
+    SetConsoleOutputCP(65001); // Türkçe karakterleri destekler
+
     StudentType students[MAX_STUDENTS];
     int studentCount = 0;
 
+    // Öğrencileri dosyadan oku
     for (int i = 0; i < MAX_STUDENTS; i++)
     {
         students[i].readFromTxt(students, studentCount, "input.txt");
     }
 
-    while (1)
+    while (true)
     {
-        cout << "1. Print all students\n";
-        cout << "2.Print students who are paid for tuition\n";
-        cout << "3. Print all students to file\n";
-        cout << "4. Exit\n";
+        cout << "0. Print all students\n";
+        cout << "1. Print students who paid for tuition\n";
+        cout << "2. Print students who did not pay for tuition\n";
+        cout << "3. Exit\n";
         cout << "Enter your choice: ";
         int choice;
         cin >> choice;
 
-        if (choice == 1)
-        {
-            for (int i = 0; i < studentCount; ++i)
-            {
-                students[i].print();
-            }
-        }
-        else if (choice == 2)
-        {
-            for (int i = 0; i < studentCount; ++i)
-            {
-                if (students[i].getIsTuitionPaid())
-                {
-                    students[i].print(1);
-                }
-            }
-        }
-        else if (choice == 3)
-        {
-            string fileName;
-            cout << "Enter file name: ";
-            cin >> fileName;
+        string fileName = "output.txt";
 
-            for (int i = 0; i < studentCount; ++i)
-            {
-                students[i].print(1, fileName);
-            }
-        }
-        else if (choice == 4)
+        switch (choice)
         {
+        case 0:
+            // Tüm öğrencileri yazdır ve dosyaya yaz
+            writeToFileAndPrint(students, studentCount, choice, fileName);
             break;
-        }
-        else
-        {
+
+        case 1:
+            // Harç ödemiş öğrencileri yazdır ve dosyaya yaz
+            writeToFileAndPrint(students, studentCount, choice, fileName);
+            break;
+
+        case 2:
+            // Harç ödemeyen öğrencileri yazdır ve dosyaya yaz
+            writeToFileAndPrint(students, studentCount, choice, fileName);
+            break;
+
+        case 3:
+            // Çıkış yap
+            return 0;
+
+        default:
             cout << "Invalid choice\n";
+            break;
         }
     }
 
